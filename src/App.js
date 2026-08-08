0ddafe827f71bd08b520eb62a913e97970306f10
Abhijit Behera <abhijitbehera710@gmail.com>
2026-08-08 23:34:40 +0530

    Create counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function increase(){
+    setCount(count + 1);
+  }
+  function decrease(){
+    setCount(count - 1);
+  }
+  function reset(){
+    setCount(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-container'>
+      <h1>Counter App</h1>
+      <div className='counter-container d-flex flex-row'>
+        <h1 className='heading'>Count</h1>
+        <h1>{count}</h1>
+        <div className='button-container d-flex flex-row'>
+          <button onClick={decrease}>Decrease</button>
+          <button style={ { color: "#959695", backgroundColor: "#e1e3e1" } } onClick={reset}>Reset</button>
+          <button style={ { color: "white", backgroundColor: "black" } } onClick={increase}>Increase</button>
+        </div>
+        <p>Use the buttons to change the counter.</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
